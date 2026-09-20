c25a036aa205b0d8588061e768c2395677053fb7
Mixagogo <mihaprih96@gmail.com>
2026-09-20 12:31:05 +0300

    обнова крч

--- Script.js
+++ Script.js
@@ -4,21 +4,21 @@ const phoneData = {
         google: { "Pixel 8 Pro": 24000, "Pixel 8": 16000, "Pixel 7 Pro": 13000 }
     };
 
-const memory = {"128 GB": 1, "256 GB": 1.15, "512 GB": 1.25, "1 TB": 1.5}
-const quality = {"Ideal": 1, "Good": 0.85 , "Okay": 0.75, "Broken": 0.3}
-const color = {"White": 1.1, "Black": 1, "Gray": 1, "Blue": 1.05, "Pink": 1.05}
+const memorylist = {"128 GB": 1, "256 GB": 1.15, "512 GB": 1.25, "1 TB": 1.5}
+const qualitylist = {"Ideal": 1, "Good": 0.85 , "Okay": 0.75, "Broken": 0.3}
+const colorlist = {"White": 1.1, "Black": 1, "Gray": 1, "Blue": 1.05, "Pink": 1.05}
 
 
 function updateModels() {
-    const brand = document.getElementById("category").value;
+    const Brand = document.getElementById("category").value;
     const modelSelect = document.getElementById("model");
     modelSelect.innerHTML = '<option value="" disabled selected>-- Оберіть модель --</option>';
         
-    if (brand && phoneData[brand]) {
+    if (Brand && phoneData[Brand]) {
         modelSelect.disabled = false;
-        Object.keys(phoneData[brand]).forEach(name => {
+        Object.keys(phoneData[Brand]).forEach(name => {
             let opt = document.createElement("option");
-            opt.value = phoneData[brand][name];
+            opt.value = phoneData[Brand][name];
             opt.textContent = name;
             modelSelect.appendChild(opt);
         });
@@ -42,35 +42,19 @@ function updateModelsbuy() {
 }
 
 function Checkprice() {
-    let Memory = document.getElementById("memory").value;
-    let Quality = document.getElementById("quality").value;
-    let Color = document.getElementById("color").value;
-    let Memorymultip = 0
-    let Qualitymultip = 0
-    let Colormultip = 0
+    const Memory = document.getElementById("memory").value;
+    const Quality = document.getElementById("quality").value;
+    const Color = document.getElementById("color").value;
+    const Model = document.getElementById("model").value;
 
-    for (i in memory) {
-        if (i == Memory){
-            Memorymultip = i
-        }
-    }
+    let Memorymultip = memorylist[Memory]
+    let Qualitymultip = qualitylist[Quality]
+    let Colormultip = colorlist[Color]
 
-    for (i in quality) {
-        if (i == Quality){
-            Qualitymultip = i
-        }
-    }
+    let price = Model * Memorymultip * Qualitymultip * Colormultip;
+    document.getElementById("pricetext").innerText = `Приблизна ціна вашого пристрою: ${price} грн`
 
-    for (i in color) {
-        if (i == Color){
-             Colormultip = i
-        }
-    }
-    
-    let price = Memorymultip * Qualitymultip * Colormultip
-
-    print(price)
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 
 
 
